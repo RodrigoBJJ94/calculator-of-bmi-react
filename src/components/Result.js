1e0fb8b440bbe5e754d248c736f60c84e7f2466d
Rodrigo Brentano <rodrigobjj24@gmail.com>
2021-09-27 19:00:44 -0300

    Version 1.03

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Result(props) {
     return (
-        <div>
+        <div className="div-result">
             <p>Result: {props.res.toFixed(2)}</p>
         </div>
     );
